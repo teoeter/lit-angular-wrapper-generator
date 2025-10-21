0fc50dc211abaf6c9c88b4ec9be1d8566addfc69
pfteter <teo.eterovic@gmail.com>
2025-10-21 21:34:34 +0200

    fix: Remove stencil test project

--- packages/angular-app/src/main.ts
+++ packages/angular-app/src/main.ts
@@ -5,14 +5,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-// Import Stencil components loader
-import { defineCustomElements } from 'stencil-components/loader';
-
 // Import Lit components to register them
 import 'lit-components';
-
-// Initialize Stencil components
-defineCustomElements();
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
